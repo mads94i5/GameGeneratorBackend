15dd262a7124dcd47f6c870916fe3645c8b5aa77
ThomasSA-Git <thom45w9@stud.kea.dk>
2023-05-04 09:55:01 +0200

    DevConfig data added

--- src/main/java/com/example/gamegenerator/config/DevConfig.java
+++ src/main/java/com/example/gamegenerator/config/DevConfig.java
@@ -50,9 +50,98 @@ public class DevConfig implements ApplicationRunner {
           .player("Multiplayer")
           .build();
 
+      GameIdea game4 = GameIdea.builder()
+          .title("League of Legends")
+          .genre("MOBA")
+          .description("Multiplayer online battle arena with champions")
+          .player("Multiplayer")
+          .build();
+
+      GameIdea game5 = GameIdea.builder()
+          .title("Final Fantasy VII Remake")
+          .genre("JRPG")
+          .description("Singleplayer role-playing game with an epic story")
+          .player("Singleplayer")
+          .build();
+
+      GameIdea game6 = GameIdea.builder()
+          .title("Fortnite")
+          .genre("Battle royale")
+          .description("Multiplayer game where players fight to be the last one standing")
+          .player("Multiplayer")
+          .build();
+
+      GameIdea game7 = GameIdea.builder()
+          .title("Civilization VI")
+          .genre("Strategy")
+          .description("Singleplayer or multiplayer game where players lead a civilization to victory")
+          .player("Singleplayer, multiplayer")
+          .build();
+
+      GameIdea game8 = GameIdea.builder()
+          .title("The Legend of Zelda: Breath of the Wild")
+          .genre("Action-adventure")
+          .description("Open-world singleplayer game with exploration and puzzles")
+          .player("Singleplayer")
+          .build();
+
+      GameIdea game9 = GameIdea.builder()
+          .title("FIFA 22")
+          .genre("Sports")
+          .description("Multiplayer game with football/soccer teams and players")
+          .player("Singleplayer, multiplayer")
+          .build();
+
+      GameIdea game10 = GameIdea.builder()
+          .title("Dark Souls III")
+          .genre("Action RPG")
+          .description("Singleplayer game with difficult enemies and challenging gameplay")
+          .player("Singleplayer")
+          .build();
+
+      GameIdea game11 = GameIdea.builder()
+          .title("The Legend of Zelda: Breath of the Wild")
+          .genre("Action-adventure")
+          .description("Open-world adventure game set in a post-apocalyptic world")
+          .player("Single player")
+          .build();
+
+      GameIdea game12 = GameIdea.builder()
+          .title("Fortnite")
+          .genre("Battle royale")
+          .description("Multiplayer game where players fight to be the last one standing")
+          .player("Multiplayer")
+          .build();
+
+      GameIdea game13 = GameIdea.builder()
+          .title("Minecraft")
+          .genre("Sandbox")
+          .description("Game about placing blocks to build anything you can imagine")
+          .player("Single player, Multiplayer")
+          .build();
+
+      GameIdea game14 = GameIdea.builder()
+          .title("Final Fantasy VII Remake")
+          .genre("Role-playing")
+          .description("Story-driven game set in a dystopian world with a focus on combat and exploration")
+          .player("Single player")
+          .build();
+
       gameRepository.save(game1);
       gameRepository.save(game2);
       gameRepository.save(game3);
+      gameRepository.save(game4);
+      gameRepository.save(game5);
+      gameRepository.save(game6);
+      gameRepository.save(game7);
+      gameRepository.save(game8);
+      gameRepository.save(game9);
+      gameRepository.save(game10);
+      gameRepository.save(game11);
+      gameRepository.save(game12);
+      gameRepository.save(game13);
+      gameRepository.save(game14);
+
     }
   }
 
